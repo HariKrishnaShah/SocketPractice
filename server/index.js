da78e0a90f512034b0ba8ddfc78dfd2098bc643c
Hari <hksshah111@gmail.com>
2023-10-01 22:27:07 +0545

    Issue: io is running twice

--- server/index.js
+++ server/index.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://0.0.0.0:27017/info')
-  .then(() => console.log('Connected!'));
+  .then(() => console.log('Mongo!'));
 const cors = require("cors");
-const Notis = require("./notification.js");
 const express = require('express')
 const app = express();
 app.use(express.static("build"));
@@ -25,41 +24,14 @@ app.get('/', function (req, res) {
 })
 app.use("/noti", require("./routes.js"));
 // jsonifying
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 app.use(cors());
-const corsOptions = {
-  origin: 'http://localhost:3000', // Replace with the origin of your frontend application
-  methods: ['GET', 'POST'],
-  credentials: true, // Include credentials (e.g., cookies) in cross-origin requests if needed
-};
-
-// // allowing CORS:
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Credentials", "true");
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET,HEAD,OPTIONS,POST,PUT,DELETE"
-//   );
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Acc" +
-//       "ess-Control-Request-Method, Access-Control-Request-Headers"
-//   );
-//   // no cache
-//   res.setHeader("Cache-Control", "no-cache");
-//   next();
-// });
-
-app.use(express.static("build"));
 server.listen(4000, ()=>{
     console.log("Listening at http://localhost:4000")
 });
 
 
 io.on("connection", async(socket) => {
-  console.log("Connected");
+  console.log("Connected io");
 
   // On connection start pushing notifications to database
   const notificationsPush = setInterval(() => {
